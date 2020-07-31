fd6fa0ade3d0557aa824abd74ee9dc9757ce343b
Mahdinv <s.mahdinavardi@gmail.com>
2020-07-31 15:43:50 +0430

    AddUserPhotoBankCardModel

---
+++ MadPay724.Data/Models/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MadPay724.Data.Models
+{
+    public class BaseEntity <T> /*bazi mavaghe masal id inte ya bite ya... ma to tamam modela ino farakhoni mikonim va oonja moshakhas mikonim ke id ma a che noei bashe. masalan ye jaei hast man mikham id ro string tariif konam bara inke primary beshe guid mikonamesh*/
+    {
+        [Key]
+        public T Id { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public DateTime DateModified { get; set; }
+    }
+}
